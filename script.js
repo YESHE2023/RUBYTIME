d92759eb7ba5d83998e2765d9ad2c7234d93e605
YESHE2023 <145490329+YESHE2023@users.noreply.github.com>
2025-05-07 03:24:11 -0500

    Crear script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarCarta() {
+  const mensaje = document.getElementById('mensaje');
+  mensaje.textContent = "Desde que estamos juntos, cada día es una nueva aventura llena de amor y alegría. Te amo con todo mi corazón.";
+}
