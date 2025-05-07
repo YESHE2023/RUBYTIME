aee194371872a5d63df9137807051411c0667894
YESHE2023 <145490329+YESHE2023@users.noreply.github.com>
2025-05-07 03:41:55 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function mostrarCarta() {
   const mensaje = document.getElementById('mensaje');
-  mensaje.textContent = "Desde que estamos juntos, cada día es una nueva aventura llena de amor y alegría. Te amo con todo mi corazón.";
+  mensaje.textContent = "Mi reina, se que estamos pasando por momentos preocupantes y difíciles, pero en ningún momento se te vaya a olvidar que te amo con todo mi corazón ❤️.";
 }
