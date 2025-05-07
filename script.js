c0e976f619574035bc66a3ed5171525818dab1b8
YESHE2023 <145490329+YESHE2023@users.noreply.github.com>
2025-05-07 03:46:47 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+let cartaMostrada = false;
+
 function mostrarCarta() {
   const mensaje = document.getElementById('mensaje');
-  mensaje.textContent = "Mi reina, se que estamos pasando por momentos preocupantes y difíciles, pero en ningún momento se te vaya a olvidar que te amo con todo mi corazón ❤️.";
+  const boton = document.querySelector('button');
+
+  if (!cartaMostrada) {
+    mensaje.textContent = "Mi reina, se que estamos pasando por momentos preocupantes y difíciles, pero en ningún momento se te vaya a olvidar que te amo con todo mi corazón ❤️.";
+    boton.textContent = "Cerrar Carta";
+    cartaMostrada = true;
+  } else {
+    mensaje.textContent = "Haz clic en el botón para leer.";
+    boton.textContent = "Leer Carta";
+    cartaMostrada = false;
+  }
 }
